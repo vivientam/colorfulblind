a7c17b74094626bea163eed1de3b5c7c22c0c913
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-02-12 17:56:18 +0800

    amend upon Harry's enquiries

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,14 +2,12 @@ class CollectionsController < ApplicationController
 
   def index
     @collections = Collection.all
-
   end
 
   def show
     @collection = Collection.find(params[:id])
 
     @apparels = @collection.apparels
-
   end
 
   def new
--- app/helpers/apparels_helper.rb
+++
@@ -1,2 +0,0 @@
-module ApparelsHelper
-end
