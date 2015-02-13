c00678fcb04c3a2e6775bd0418eba320e5d068c5
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-02-13 23:48:48 +0800

    set up the FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
 
   get 'appointment' => 'static_pages#appointment'
 
+  get 'faq' => 'static_pages#faq'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
