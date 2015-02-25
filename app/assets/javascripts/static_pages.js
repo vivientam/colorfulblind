01f5c89a567e9b97b82f3b8a651c9efa819db4b3
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-02-25 15:44:08 +0800

    added onclick function to size-option button to have a highlight black background

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// function sizeOption() {
+//   // alert ("Hello!");
+
+// }
+$(document).ready(function(){
+
+  $(".size-option").on("click", function(){
+    $(this).addClass("black");
+  })
+})
