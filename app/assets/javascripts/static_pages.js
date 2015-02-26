67b46daf64b2fe167fb4c828e579bab92f423b05
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-02-26 08:24:17 +0800

    modified js css apparels/show file for sizes

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -7,6 +7,10 @@
 $(document).ready(function(){
 
   $(".size-option").on("click", function(){
+    // Select all items of the same class, and remove the class "black"
+    $(".size-option").removeClass("black");
+    
+    // Add the class "black" to the selected button
     $(this).addClass("black");
   })
 })
