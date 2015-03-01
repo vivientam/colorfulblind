cb8ddce613d757c4a16c4a3161382aa4d52d8477
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-03-01 11:20:47 +0800

    deleted payments_helper.rb and changed the default email to current user email.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,8 +6,8 @@ class PaymentsController < ApplicationController
 
   def create
     begin
-      @amount = 500
-      customer = Stripe::Customer.create(:email => 'example@stripe.com', :card  => params[:stripeToken])
+      @amount = 0
+      customer = Stripe::Customer.create(:email => current_user.email, :card  => params[:stripeToken])
       charge = Stripe::Charge.create(:customer    => customer.id, :amount      => @amount, :description => 'Rails Stripe customer', :currency    => 'HKD')
     rescue Stripe::CardError => e
       flash[:error] = e.message
