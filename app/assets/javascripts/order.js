959a9177813648335fbaff8c49f0e8b582f584ab
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-03-02 11:45:11 +0800

    changed class subtracted to removeClass

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   $(".button").on("click", function(){
     // Select all items of the same class, and remove the class "black"
-    $(".button").subtract("black");
+    $(".button").removeClass("black");
     
     // Add the class "black" to the selected button
     $(this).addClass("black");
